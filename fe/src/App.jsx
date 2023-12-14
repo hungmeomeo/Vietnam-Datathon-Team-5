98b8f6ef71de0bb7852552c15bf65401a99c1fa1
hungmeomeo <hungdevhcmut@gmail.com>
2023-12-14 19:02:26 +0700

    First commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./page/Homepage/Homepage";
+
+const App = () => {
+  return (
+    <div>
+      <Homepage></Homepage>
+    </div>
+  );
+};
+
+export default App;
