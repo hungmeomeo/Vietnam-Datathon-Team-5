0ce78da39ef495edc7796889c560007c9aae7e0e
hungmeomeo <hungdevhcmut@gmail.com>
2023-12-16 12:32:52 +0700

    Upadte chartJs

--- fe/src/components/chart/line/line.jsx
+++ fe/src/components/chart/line/line.jsx
@@ -33,6 +33,27 @@ const options = {
       display: true,
       text: "Chart.js Line Chart",
     },
+    tooltip: {
+      enabled: true,
+      mode: "index",
+      intersect: false,
+    },
+  },
+  scales: {
+    x: {
+      display: true,
+      title: {
+        display: true,
+        text: "Month",
+      },
+    },
+    y: {
+      display: true,
+      title: {
+        display: true,
+        text: "Value",
+      },
+    },
   },
 };
 
@@ -41,13 +62,13 @@ const data = {
   datasets: [
     {
       label: "Dataset 1",
-      data: [500, 700, 300, 800, 400, 900, 600], // Replace with your actual data
+      data: [500, 700, 300, 800, 400, 900, 600],
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Dataset 2",
-      data: [700, 300, 600, 400, 900, 500, 800], // Replace with your actual data
+      data: [700, 300, 600, 400, 900, 500, 800],
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
