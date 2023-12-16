71d9ffa2e36714ba0e59a3b0a87779c45ec89ec7
hungmeomeo <hungdevhcmut@gmail.com>
2023-12-16 11:48:42 +0700

    Adding chart

---
+++ fe/src/components/chart/line/line.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [500, 700, 300, 800, 400, 900, 600], // Replace with your actual data
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: [700, 300, 600, 400, 900, 500, 800], // Replace with your actual data
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+const LineChart = () => <Line options={options} data={data} />;
+
+export default LineChart;
